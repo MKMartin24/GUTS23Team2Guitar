3e1562fd5e6456e193ead02e9b4775291ff25384
Khushi761 <khushbooaswani20@gmail.com>
2023-10-28 18:21:41 +0100

    base.html error to be fixed

--- guitarguitar/urls.py
+++ guitarguitar/urls.py
@@ -4,18 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "guitarguitar"
-
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
-    path('/orders/', views.view_orders, name='view_orders'),
-    path('/customers/', views.view_customers, name='view_customers'),
-    path('/products/', views.view_products, name='view_products'), 
-=======
     path('orders/', views.view_orders, name='view_orders'),
     path('customers/', views.view_customers, name='view_customers'),
     path('products/', views.view_products, name='view_products'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
->>>>>>> refs/remotes/origin/master
 ]
