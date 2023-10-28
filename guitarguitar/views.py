3911c7bf258153ff545042114d736f83758260f1
Mairi Martin <mairi.kate0815@gmail.com>
2023-10-28 11:42:12 +0100

    Data linked to webpage

--- guitarguitar/views.py
+++ guitarguitar/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from guitarguitar.data import Order
 
 # Create your views here.
 def index(request):
-    context_dict = {}
+    context_dict = {"orders":Order.load_api()}
+    print(context_dict)
     return render(request, 'guitarguitar/index.html', context=context_dict)
