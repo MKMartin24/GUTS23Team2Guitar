57ccdb83eb44de3263c5c8d3252778273e795446
Mairi Martin <mairi.kate0815@gmail.com>
2023-10-28 10:39:44 +0100

    added pdf spec

--- guitarguitar/views.py
+++ guitarguitar/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
+def index(request):
+    context_dict = {}
+    return render(request, 'guitarguitar/index.html', context=context_dict)
