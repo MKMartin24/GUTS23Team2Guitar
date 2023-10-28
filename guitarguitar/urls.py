0d92a672e8ce1f7c871a14bc5cd7b699ec0c6857
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-10-28 12:55:49 +0100

    Fix urls warning

--- guitarguitar/urls.py
+++ guitarguitar/urls.py
@@ -7,9 +7,9 @@ app_name = "guitarguitar"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/orders/', views.view_orders, name='view_orders'),
-    path('/customers/', views.view_customers, name='view_customers'),
-    path('/products/', views.view_products, name='view_products'),
+    path('orders/', views.view_orders, name='view_orders'),
+    path('customers/', views.view_customers, name='view_customers'),
+    path('products/', views.view_products, name='view_products'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
